1b7a0d5587dd2d4c0a3596defdabd2c9ebae0c1d
Julius Jansson <julius.jansson@elev.ga.ntig.se>
2024-05-23 10:49:17 +0200

    oihfefaw

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -11,84 +11,19 @@ Raylib.SetTargetFPS(60);
 
 Player player = new Player();
 WallsC wall = new WallsC();
+Scene scene = new Scene();
 
-
-string scene = "start";
+// string scene = "start";
 
 // logic for 3D array
 wall.Logic();
 
 while (!Raylib.WindowShouldClose())
 {
-    // if start then draw and write this
-    if (scene == "start")
-    {
-        Raylib.ClearBackground(Color.Yellow);
-        Raylib.DrawText("Press Space to play", 150, 310, 35, Color.Black);
-        Raylib.DrawText("A and D to move Space to jump", 100, 370, 35, Color.Black);
-        Raylib.DrawText("Avoid the dark green rectangles and try to reach the purple", 50, 430, 35, Color.Black);
-        if (Raylib.IsKeyPressed(KeyboardKey.Space))
-        {
-            // changes scene to play
-            scene = "play";
-            // game = true;
-
-        }
-    }
-
-    if (scene == "play")
-    {
-        // movement method
-        player.Move(wall);
-        // draws 3D array
-        wall.Draw();
-
-
-        Raylib.BeginDrawing();
-        Raylib.ClearBackground(Color.SkyBlue);
-        // draws character
-        Raylib.DrawRectangleRec(player.Character, Color.Purple);
-
-        // if wall and gubbe collide change scene
-        Collision.Check(wall, player);
-        scene = Collision.Games;
-
-        // used for testing jump
-        // Console.WriteLine(Collision.hopp);
-
-    }
-    // when dead
-    if (scene == "dead")
-    {
-        Raylib.ClearBackground(Color.Yellow);
-        Raylib.DrawText("Press space to try again", 150, 310, 35, Color.Black);
-        Raylib.DrawText("I SAID AVOID THE DARK GREEN RECTANGLES", 120, 370, 35, Color.Black);
-
-        if (Raylib.IsKeyPressed(KeyboardKey.Space))
-        {
-            // If you press space game is restarted
-            player.Character.Y = 500;
-            player.Character.X = 100;
-            scene = "play";
-        }
-    }
-    // if you somehow manage to win
-    if (scene == "win")
-    {
-        Raylib.ClearBackground(Color.Yellow);
-        Raylib.DrawText("WOW, YOU WIN", 150, 310, 35, Color.Black);
-        Raylib.DrawText("Press Space to play again", 150, 350, 35, Color.Black);
-        Raylib.DrawText("Press ESC to exit", 150, 400, 35, Color.Black);
-
-        if (Raylib.IsKeyPressed(KeyboardKey.Space))
-        {
-            // also restarts the game. If you want to play again
-            player.Character.Y = 500;
-            player.Character.X = 100;
-            scene = "play";
-        }
-
-    }
+   
+   //changes scenes 
+   scene.Window(wall, player);
+   
 
     Raylib.EndDrawing();
 }
